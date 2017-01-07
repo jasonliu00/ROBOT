06377e8c22bc28ba9e6e1463d82a649264031160
liulizhao <liuliz123@163.com>
2017-01-07 21:17:47 +0800

    add QSqlDatabase but not work

--- ROBOT/main.cpp
+++ ROBOT/main.cpp
@@ -1,9 +1,28 @@
 #include "robotmainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
+#include <QSqlError>
+
+bool createConnection()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("ROBOT");
+    db.setUserName("liulizhao");
+    db.setPassword("123456");
+    if(!db.open()){
+        QMessageBox::critical(0, QObject::tr("Database Error"),
+                              db.lastError().text());
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if(!createConnection())
+        return 1;
     RobotMainWindow w;
     w.setWindowIcon(QIcon(":/images/title"));
     w.show();
